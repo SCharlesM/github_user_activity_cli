a4b101da013c4b2d9c11131c3be7e888e970a4f2
SCharles <97812129+SCharlesM@users.noreply.github.com>
2024-11-10 20:54:06 +0000

    finished basic version

--- github_user_activity.py
+++ github_user_activity.py
@@ -13,43 +13,54 @@ def main():
 
     #if length of sys.argv is 0, say its too short...
     if len(sys.argv) < 2 :
-        print('no arguments provided, please add a github username as an argument')
+        print('No arguments provided, please add a github username as an argument')
+
     elif len(sys.argv) > 2 :
-        print('too many arguments, please add 1 github username as an argument')
+        print('Too many arguments, please add 1 github username as an argument')
+
     else :
         username = sys.argv[1]
         print('username: ', username)
 
-    #set the access token
-    #access_token = 'YOUR_API_TOKEN'
+        #set the access token
+        #access_token = 'YOUR_API_TOKEN'
 
-    #set the api endpoint
-    url = f'https://api.github.com/users/{username}/events'
-    #edit this to add username....
-    #print('this is the url: https://api.github.com/users/',username,'/events')
+        #set the api endpoint
+        url = f'https://api.github.com/users/{username}/events'
 
-    #send the GET request
-    response = requests.get(url, timeout = 10)
+        #send the GET request
+        response = requests.get(url, timeout = 10)
 
-    print(response)
+        print(response)
 
-    #check the response status code
-    if response.status_code == 200:
-        #parse the JSON response
-        events = response.json()
+        #check the response status code
+        if response.status_code == 200:
+            #parse the JSON response
+            events = response.json()
 
-        #print each event type and created at time
-        for index, event in enumerate(events):
-            if index > 4:
-                break
-            print(f"Event: {event['type']}")
+            #print latest 5 event types
+            for index, event in enumerate(events):
+                if index > 4:
+                    break
+                print(f"Event: {event['type']}")
 
-    else :
-        print(f'Error: {response.status_code}')
+        elif response.status_code == 400 :
+            print(f'Error: {response.status_code} - The request was invalid or cannot be processed')
 
-    #deal with the returned status code....
+        elif response.status_code == 401 :
+            print(f'Error: {response.status_code} - Authentication '
+                'is required and hasn not been provided or is invalid')
 
-    #read the returned json data and print....
+        elif response.status_code == 403 :
+            print(f'Error: {response.status_code} - The '
+                'request is valid but the client does not have the necessary permissions''')
+
+        elif response.status_code == 404 :
+            print(f"Error: {response.status_code} - The "
+                "requested resources doesn't exist, is the username correct?")
+
+        else :
+            print(f'Error: {response.status_code}')
 
 if __name__ == "__main__" :
     main()
